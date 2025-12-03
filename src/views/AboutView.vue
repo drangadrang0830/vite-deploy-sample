6ff2da48062a08c61c31bc7e460d2f37e862a3f9
drangadrang0830 <boss0830@hotmail.com.tw>
2025-12-03 10:19:00 +0800

    add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+    }
+  },
+  mounted() { // 引用環境變數
+    console.log(import.meta.env.VITE_TEXT)
+    const url = import.meta.env.VITE_PATH // 引用環境變數
+    this.$http.get(url)
+      .then(res => {
+        // console.log(res.data.results[0]);
+        this.data = res.data.results[0]; //賦值 回傳檔 給 data
+        // console.log(this.data)
+      })
+  },
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/AttributeProperty.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>
+      <span :title="aaa">
+        Hover your mouse over me for a few seconds to see my dynamically bound title!
+      </span>
+    </p>
+    <p :class="{ red: bbb }" @click="ddd">
+      This should be red... but click me to toggle it.
+    </p>
+    <p :style="{ color: ccc }" @click="eee">
+      This should be green, and should toggle between green and blue on click.
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const aaa = ref('Hello World!')
+const bbb = ref('true')
+const ccc = ref('yellow')
+
+function ddd() {//每次觸發開關紅色類別
+  bbb.value = !bbb.value
+}
+
+function eee() {//每次觸發判斷是否為綠色 若是 為藍色 若否 為綠色
+  ccc.value = ccc.value === 'yellow' ? 'pink' : 'yellow'
+}
+
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
